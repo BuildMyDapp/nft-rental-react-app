db56208f520b176b21ba195619f844f8fccfeae6
reactDev777 <reactdevs777@gmail.com>
2021-10-13 02:59:21 +0500

    faq screen added

--- src/components/exportComponents.js
+++ src/components/exportComponents.js
@@ -2,5 +2,5 @@ export { default as Rent } from "./rent/index";
 export { default as Error404 } from "./404/index";
 export { default as Lend } from "./lend/Index";
 export { default as Dashboard } from "./dashboard/index";
-export { default as Faq } from "./faq/index";
+export { default as Faq } from "./faq/Index";
 export { default as Header } from "./header/Index";
